b98a042e0f003c0b14e62e565c909655840af0ac
James Rubingh <james@wrive.com>
2018-03-14 15:10:32 +0900

    use a function
    Signed-off-by: James Rubingh <james@wrive.com>

--- scrape.js
+++ scrape.js
@@ -1,20 +1,26 @@
 // Scrape data from OpenText summary page for copying and pasting into translations
 // by James Rubingh 3/14/2018
 
+//load Palanquin font from google
 $('head').append('<link href="https://fonts.googleapis.com/css?family=Montserrat|Palanquin" rel="stylesheet" />');
+
+function ots_show_text(cell_num, get_html = false) {
+  element = $('tr.browseRow1 td:nth-child('+cell_num+')')
+  return get_html ? element.html() : element.text();
+};
 
 $('#SearchForm').after(
   "<div style=\"background-color:#dfe9f0;margin:8px;padding:5px;border:1px solid #B3CBDC;font-family:\'Palanquin\', sans-serif;line-height: 1.5em;font-size:1.3em;\"><b>TITLE</b>:<br/>&nbsp; &nbsp;jp:&nbsp; &nbsp; &nbsp; &nbsp;" +
-    $('tr.browseRow1 td:nth-child(29)').text() +
+    ots_show_text(29) +
   "<br/>&nbsp; &nbsp;en:&nbsp; &nbsp; &nbsp; &nbsp; " +
-  $('tr.browseRow1 td:nth-child(30)').text() +
+  ots_show_text(30) +
   "<br/>&nbsp;<br/><b>OWNER</b>:<br/>&nbsp; &nbsp;jp:&nbsp; &nbsp; &nbsp; &nbsp;" +
-  $('tr.browseRow1 td:nth-child(14)').text() +
+  ots_show_text(14) +
   "<br/>&nbsp; &nbsp;en:&nbsp; &nbsp; &nbsp; &nbsp; " +
-  $('tr.browseRow1 td:nth-child(15)').text() +
+  ots_show_text(15) +
   "<br/><p><b>Application No.</b>: &nbsp; " +
-  $('tr.browseRow1 td:nth-child(21)').text().replace('特願','') +
+  ots_show_text(21).replace('特願','') +
   "<br/><p><b>Related Case Numbers</b>:" +
-  $('tr.browseRow1 td:nth-child(32)').html() +
+  ots_show_text(32, true) +
   "</p></div>"
 );
